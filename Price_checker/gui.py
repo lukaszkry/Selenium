9606c9b3d7d7d9946cffbc47b2a33e54402f280b
Łukasz Kryścio <lukasz.kryscio@gmail.com>
2021-05-27 23:19:53 +0200

    GUI in progress price_checker.py changes in functions

--- Price_checker/gui.py
+++ Price_checker/gui.py
@@ -1,8 +1,118 @@
-from Price_checker import price_checker
+import price_checker
 from tkinter import *
 
-class Box:
 
-    def  __init__(self):
-        self.item_list = ['mirror shard', 'headhunter']
+class MainWindow:
+
+    def __init__(self, master):
+        self.master = master
+        self.item_list = ['mirror shard', 'headhunter', 'cos']
         self.currency_type_list = ['Exalted Orb', 'chaos orb']
+        # Class from price_checker.py
+        self.check = price_checker.Price()
+
+        # Labels
+        self.space_label = Label(self.master, text=' ', padx=15, pady=20)
+        self.space_label.grid(row=0, column=0)
+        self.item_label = Label(self.master, text='Choose item from list', padx=30, pady=20)
+        self.item_label.grid(row=0, column=1)
+        self.currency_type_label = Label(self.master, text='Choose currency type', padx=30, pady=20)
+        self.currency_type_label.grid(row=0, column=2)
+        self.threshold_label = Label(self.master, text='Threshold', padx=20, pady=20)
+        self.threshold_label.grid(row=0, column=4)
+        self.sell_label = Label(self.master, text='Sell', padx=20, pady=20)
+        self.sell_label.grid(row=1, column=3)
+        self.buy_label = Label(self.master, text='Buy', padx=20, pady=20)
+        self.buy_label.grid(row=2, column=3)
+        self.refresh_rate_label = Label(self.master, text='Refresh rate (s)', padx=20, pady=20)
+        self.refresh_rate_label.grid(row=0, column=5)
+        self.refresh_repetitions_label = Label(self.master, text='Number of refreshes', padx=20, pady=20)
+        self.refresh_repetitions_label.grid(row=2, column=5)
+
+        # Entries
+        self.item_entry = Entry(self.master, width=25)
+        self.item_entry.grid(row=1, column=1)
+        self.currency_type_entry = Entry(self.master, width=25)
+        self.currency_type_entry.grid(row=1, column=2)
+        self.sell_entry = Entry(self.master, width=20)
+        self.sell_entry.grid(row=1, column=4)
+        self.buy_entry = Entry(self.master, width=20)
+        self.buy_entry.grid(row=2, column=4)
+        self.refresh_rate_entry = Entry(self.master, width=20)
+        self.refresh_rate_entry.grid(row=0, column=6)
+        self.number_of_refreshes_entry = Entry(self.master, width=20)
+        self.number_of_refreshes_entry.grid(row=2, column=6)
+
+        # Listbox
+        self.item_listbox = Listbox(self.master, width=25)
+        self.item_listbox.grid(row=2, column=1)
+        self.currency_type_listbox = Listbox(self.master, width=25)
+        self.currency_type_listbox.grid(row=2, column=2)
+
+        # Buttons
+        self.start_button = Button(self.master, text='START', padx=20, pady=20, command=self.compare)
+        self.start_button.grid(row=4, column=6)
+
+    def update_item(self, data):
+        self.item_listbox.delete(0, END)
+        for item in data:
+            self.item_listbox.insert(END, item)
+
+    def update_currency_type(self, data):
+        self.currency_type_listbox.delete(0, END)
+        for item in data:
+            self.currency_type_listbox.insert(END, item)
+
+    def fill_item_entry(self, event):
+        self.item_entry.delete(0, END)
+        self.item_entry.insert(0, self.item_listbox.get(ANCHOR))
+
+    def fill_currency_type_entry(self, event):
+        self.currency_type_entry.delete(0, END)
+        self.currency_type_entry.insert(0, self.currency_type_listbox.get(ANCHOR))
+
+    def check_item(self, event):
+        typed = self.item_entry.get()
+        if typed == '':
+            data = self.item_list
+        else:
+            data = []
+            for item in self.item_list:
+                if typed.lower() in item.lower():
+                    data.append(item)
+        self.update_item(data)
+
+    def check_currency_type(self, event):
+        typed = self.currency_type_entry.get()
+        if typed == '':
+            data = self.currency_type_list
+        else:
+            data = []
+            for item in self.currency_type_list:
+                if typed.lower() in item.lower():
+                    data.append(item)
+        self.update_currency_type(data)
+
+    # def get_data_from_site(self):
+    #     return self.check.check()
+
+    def compare(self):
+        buy = self.buy_entry.get()
+        sell = self.sell_entry.get()
+        item = self.item_entry.get()
+        data = self.check.check(item)
+        self.check.compare(data, buy, sell)
+        self.check.finish()
+
+    def execute(self):
+        self.update_item(self.item_list)
+        self.item_listbox.bind("<<ListboxSelect>>", self.fill_item_entry)
+        self.item_entry.bind("<KeyRelease>", self.check_item)
+        self.update_currency_type(self.currency_type_list)
+        self.currency_type_listbox.bind("<<ListboxSelect>>", self.fill_currency_type_entry)
+        self.currency_type_entry.bind("<KeyRelease>", self.check_currency_type)
+
+main_window = Tk()
+app = MainWindow(main_window)
+app.execute()
+main_window.mainloop()
--- Price_checker/price_checker.py
+++ Price_checker/price_checker.py
@@ -12,16 +12,17 @@ class Price:
     def __init__(self):
         path = 'msedgedriver.exe'
         self.driver = webdriver.Edge(path)
-        self.item = 'mirror shard'
+        self.item = 'exalted orb'
         self.currency_type = 'Exalted Orb'
-        self.currency_threshold = 13
+        self.buy_threshold = '13'
+        self.sell_threshold = '14'
 
 # Function checking if there are any offers cheaper than set threshold
-    def check(self):
+    def check(self, item):
         self.driver.get('https://www.pathofexile.com/trade/search/Ultimatum')
         time.sleep(5)    # try WebdriverWait method is not working for some reason (element is located but I can't interact with it)
         text_field = self.driver.find_element_by_class_name('multiselect__input')
-        text_field.send_keys(self.item)
+        text_field.send_keys(item)
         text_field.send_keys(Keys.RETURN)
         search_button = self.driver.find_element_by_class_name('controls')
         button = search_button.find_element_by_tag_name('span')
@@ -36,26 +37,32 @@ class Price:
         time.sleep(5)
         rows = table.find_element_by_class_name('row')
         price = rows.find_elements_by_tag_name('span')
-        data = [price[0].text, price[9].text, price[12].text]
+        for row in price:
+            print(row.text)
+        if price[0].text == '':   # data = [amount, item price, currency type, item type]
+            data = [1, price[-8].text, price[-5].text, price[1].text]
+        else:
+            data = [price[0].text, price[-8].text, price[-5].text, price[2].text]
         return data
 
-    def compare(self, data):
-        if data[2].lower() == self.currency_type.lower():
-            if float(data[1]) <= self.currency_threshold:
-                print(f'Buy {data[0]} for {float(data[0])*float(data[1])} {data[2]}')
-            else:
-                print("Too expensive!")
-        else:
-            print('Wrong currency type!')
+    def compare(self, data, buy, sell):
+        if buy == '':
+            pass
+        elif float(data[1]) <= float(buy):
+            print(f'Buy {data[0]} {data[3]} for {float(data[0])*float(data[1])} {data[2]}')
+        elif sell == '':
+            pass
+        elif float(data[1]) >= float(sell):
+            print(f'Sell {data[0]} {data[3]} for {float(data[0])*float(data[1])} {data[2]}')
 
     def finish(self):
         self.driver.quit()
 
     def execute(self):
-        data = self.check()
+        data = self.check(self.item)
         print(data)
-        self.compare(data)
+        self.compare(data, self.buy_threshold, self.sell_threshold)
         self.finish()
 
-
-check = Price()
+if __name__ == '__main__':
+    check = Price()
