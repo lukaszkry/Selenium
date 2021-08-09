3b4495595a0f294156c27605911c42abf9d51ed0
theri <lukasz.kryscio@gmail.com>
2021-08-09 20:03:08 +0200

    changes in praca.py

--- Calculator_2.0/praca.py
+++ Calculator_2.0/praca.py
@@ -3,7 +3,27 @@
 # ioreg -l | grep IOPlatformSerialNumber#Mac OS X
 import os
 import sys
+import subprocess
 
+def create_dir(dir):
+  if not os.path.exists(dir):
+    os.makedirs(dir)
+    print("Created Directory : ", dir)
+  else:
+    print("Directory already existed : ", dir)
+  return dir
+
+
+def create_txt(path):
+    os.chdir(path)
+    with open('monitors.txt', 'w') as fp:
+        pass
+
+def run_ps1():
+    p = subprocess.Popen(["powershell.exe",
+                          "C:\\Users\\theri\PycharmProjects\\Selenium\\Calculator_2.0\\test.ps1"],
+                         stdout=sys.stdout)
+    p.communicate()
 
 def getMachine_addr():
     os_type = sys.platform.lower()
@@ -15,7 +35,18 @@ def getMachine_addr():
         command = "ioreg -l | grep IOPlatformSerialNumber"
     return os.popen(command).read().replace("\n", "")
 
+def user_name():
+    os_type = sys.platform.lower()
+    return os.popen("whoami").read().replace("\n", "").replace("\\", "  ")
+
+def ram_clock():
+    os_type = sys.platform.lower()
+    return os.popen("wmic memorychip get speed").read().replace("\n", "").replace("\\", "  ")
 
 # output machine serial code: XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXX
+create_dir('C:\\test')
+create_txt('C:\\test')
+run_ps1()
 print(getMachine_addr())
-
+print(user_name())
+print(ram_clock())
